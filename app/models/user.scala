503ae14a5f1ebf0f37465e9f68172472cbd2ceb9
mawentao007 <1320963812@qq.com>
2015-05-24 02:22:47 +0800

    bad

--- app/models/user.scala
+++ app/models/user.scala
@@ -1,7 +1,7 @@
 package models
 
 
-
+import org.slf4j.{LoggerFactory, Logger}
 import play.api.libs.json._
 import services.MongoConnect._
 
@@ -30,20 +30,22 @@ object JsonFormats {
 object User{
   import JsonFormats._
   import play.api.libs.concurrent.Execution.Implicits.defaultContext
+  private final val logger: Logger = LoggerFactory.getLogger(classOf[User])
 
   private val duration = Duration(100,"ms")
   private def collection = getCollection("users")
 
   def authenticate(firstname:String,lastname:String):Option[User] = {
-    /* val userList = collection.find(Json.obj("firstname" -> firstname,"lastname"->lastname)).cursor[User]
-    .collect[List](1)
-    val user = userList.map{
-      user =>
-        user.head
-    }
-    Some(Await.result(user,duration))
-  }*/
-    Some(User(12, firstname, lastname, true))
+   val userList:Future[List[User]] = collection.find(Json.obj("firstname" -> firstname,"lastname"->lastname)).cursor[User]
+    .collect[List]()
+ //   Duration.Inf  无限等待
+    val result:List[User]  = Await.result(userList,Duration.Inf)
+    if(result.isEmpty) return None
+    Some(result.head)
+    //Some(User(12,"12","12",true))
+
   }
+
+
 
 }
