2a7e61f51a4093b0a3ca6717253d2496f7568599
mawentao007 <1320963812@qq.com>
2015-05-28 22:32:15 +0800

    post picture prime

--- app/controllers/Post/Post.scala
+++ app/controllers/Post/Post.scala
@@ -1,17 +1,16 @@
 package controllers.Post
 
-import controllers.Application
-import controllers.Secured
+import controllers. Secured
 import play.modules.reactivemongo.MongoController
-import play.modules.reactivemongo.json.collection.JSONCollection
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
-import reactivemongo.api.Cursor
+import reactivemongo.api.gridfs.{ReadFile, GridFS}
+import reactivemongo.bson.{BSONObjectID, BSONDocument, BSONValue}
+import scala.concurrent.Future
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.Logger
-import javax.inject.Singleton
 import play.api.mvc._
-import play.api.libs.json._
+//没有就报错
+import reactivemongo.api.gridfs.Implicits.{DefaultReadFileReader}
+
+
 
 
 /**
@@ -19,25 +18,57 @@ import play.api.libs.json._
  */
 class Post extends Controller with MongoController with Secured {
 
+  val gridFS = GridFS(db)
 
-  def upload = Action(parse.multipartFormData) { request =>
-    request.body.file("picture").map { picture =>
-      import java.io.File
-      val filename = picture.filename
-      val contentType = picture.contentType
-      picture.ref.moveTo(new File(s"/tmp/picture/$filename"))
-      Ok("File uploaded")
-    }.getOrElse {
-      Redirect(controllers.routes.Application.index).flashing(
-        "error" -> "Missing file")
+  def post = Action.async(gridFSBodyParser(gridFS)) { request =>
+    // here is the future file!
+    //   val futureFile:Future[[ReadFile[BSONValue]]] = request.body.files.head.ref
+    //将数据存入gridfs，同时返回存储后的元数据
+    val futureFile: Future[ReadFile[BSONValue]] = request.body.files.head.ref
+    // when the upload is complete, we add the article id to the file entry (in order to find the attachments of the article)
+/*    val futureUpdate = for {
+      file <- futureFile
+      // here, the file is completely uploaded, so it is time to update the article
+      updateResult <- {
+        //从元数据集合中找到相应的文件的元数据并更新
+        //Genericcollection有update方法，BSONCollection继承了这个trait。
+        gridFS.files.asInstanceOf[BSONCollection].update(
+          BSONDocument("_id" -> file.id),
+          BSONDocument("$set" -> BSONDocument("article" -> BSONObjectID(id))))
+      }
+    } yield updateResult*/
+
+    futureFile.map {
+      case file => Redirect(controllers.Post.routes.Post.getAllPost())
+    }.recover {
+      case e => InternalServerError(e.getMessage())
     }
   }
 
 
-  //别忘了加render()
-  def uploadPage = Action.async{
-    Future(Ok(views.html.Post.post.render()))
+
+  def getPostById(id:String) = Action.async{
+    request =>
+      val file = gridFS.find(BSONDocument("_id" -> BSONObjectID(id)))
+      serve(gridFS,file)
+  }
+
+  def getAllPost =Action.async{
+    gridFS.find(BSONDocument()).collect[List]().map{
+      files =>
+        val filesWithId = files.map { file =>
+          file.id.asInstanceOf[BSONObjectID].stringify -> file.asInstanceOf[ReadFile[BSONValue]]
+        }
+        Ok(views.html.Post.post(Some(filesWithId)))
+    }
+
 
   }
+
+ /* def postPage = Action.async{
+    request =>
+      Future.successful(Ok(views.html.Post.post.render()))
+  }*/
+
 
 }
